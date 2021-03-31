0856342045d6976de96e29530e9c3bdbeee67a88
Natalya Myunster <natalyamyunster@yahoo.com>
2021-03-31 22:42:46 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// let a = 'java';
+// let b = 'script';
+// alert(a + '' + b);
+
+
+// let age = 13;
+// alert('me ' + age + ' years');
+
+
+// let str = 'cake';
+// alert(str.length);
+
+let str = `a
+b
+c`;
+alert(str);
